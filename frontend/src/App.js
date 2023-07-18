ba914d0d4d1652469962be510bf5830ef20f077f
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-07-18 10:10:19 +0100

    setting up frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import LoginPage from './containers/LoginPage';
+
+function App() {
+
+
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <LoginPage/>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
