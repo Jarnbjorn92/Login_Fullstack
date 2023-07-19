5897a7fd4289f6e483e4623439b4c09bc32949c7
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-07-19 14:01:17 +0100

    fixed django dashboard issue. something to do with debug and allow list

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-94&p2ms#*s327xl6bm(n&^#3^h3p!dh6e978^0c(638s_bt$8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
